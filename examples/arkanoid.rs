514bcd4069affe07b64493a4170b512ca48400df
Christophe MASSOLIN <christophe.massolin@gmail.com>
2020-04-18 01:43:50 +0200

    youpi

--- examples/arkanoid.rs
+++ examples/arkanoid.rs
@@ -3,24 +3,34 @@ use fine::{
     wasm::{canvas, console},
 };
 
-struct Stage;
+struct Stage {
+    width: i32,
+    height: i32,
+    x: i32
+}
 
 impl event::EventHandler for Stage {
     fn frame(&mut self) {
         canvas::clear();
         canvas::fill_style("red");
-        canvas::fill_rect(0, 0, 100, 200);
+        canvas::fill_rect((self.x - 50) as usize, (self.height - 20) as usize, 100, 20);
     }
 
     fn resize(&mut self, width: i32, height: i32) {
+        self.width = width;
+        self.height = height;
         console::log(format!("Resolution {}x{} from Stage", width, height).as_str())
     }
 
     fn pointer_move(&mut self, x: i32, y: i32) {
-        console::log(format!("{}x{}", x, y).as_str());
+        self.x = x;
     }
 }
 
 pub fn main() {
-    start(Stage);
+    start(Stage {
+        width: 0,
+        height: 0,
+        x: 0,
+    });
 }
